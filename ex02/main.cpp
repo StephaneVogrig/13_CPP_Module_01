2394b36e737b628e10da6f494f40a587ccdc1c1d
StephaneVogrig <stephane.vogrig@gmail.com>
2025-02-16 18:51:41 +0100

    add ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/16 15:10:09 by svogrig           #+#    #+#             */
+/*   Updated: 2025/02/16 18:50:12 by svogrig          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+
+int main(void)
+{
+	std::string		string = "HI THIS IS BRAIN";
+	std::string*	stringPTR = &string;
+	std::string&	stringREF = string;
+
+	std::cout << "memory adress of 'string'    : " << &string << std::endl;
+	std::cout << "memory adress of 'stringPTR' : " << stringPTR << std::endl;
+	std::cout << "memory adress of 'stringREF' : " << &stringREF << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << "value of the string 'string'    : " << string << std::endl;
+	std::cout << "value pointed to by 'stringPTR' : " << *stringPTR << std::endl;
+	std::cout << "value pointed to by 'stringREF' : " << stringREF << std::endl;
+
+	return (EXIT_FAILURE);
+}
