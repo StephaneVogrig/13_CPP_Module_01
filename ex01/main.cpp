61b8b9b7cea7885ef4ff7c4942be45f4fbc0a973
Stephane Vogrig <svogrig@3E6.42angouleme.fr>
2025-02-20 15:47:00 +0100

    reforge test for ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,22 +6,65 @@
 /*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 14:11:02 by svogrig           #+#    #+#             */
-/*   Updated: 2025/02/19 20:47:28 by svogrig          ###   ########.fr       */
+/*   Updated: 2025/02/20 15:45:46 by svogrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <string>
 #include "Zombie.hpp"
 
-#define NBR_ZOMBIE 10
+# define RED "\033[31m"
+# define GREEN "\033[32m"
+# define YELLOW "\033[33m"
+# define RESET "\033[0m"
 
-int main(void)
+int	strToInt(char *str)
 {
-	Zombie *horde = zombieHorde(NBR_ZOMBIE, "zomzom");
-	if (horde == NULL)
+	char* endptr;
+	long int nbr;
+	nbr = strtol(str, &endptr, 10);
+	if (endptr == str || *endptr != '\0')
+	{
+		std::cerr << RED "Error: " RESET << str << YELLOW " invalid number" RESET << std::endl;
+		exit (EXIT_FAILURE);
+	}
+	if (nbr < 0 || nbr > 50)
+	{
+		std::cerr << RED "Error: " RESET << str << YELLOW " out of range [0 - 50]" RESET << std::endl;
+		exit (EXIT_FAILURE);
+	}
+	return ((int)nbr);
+}
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		std::cerr << RED "Error:" YELLOW " wrong number of arguments !" RESET << std::endl;
+		std::cerr << YELLOW "Usage: "  RESET << argv[0] << " <number of zombies in the horde>" << std::endl;
 		return (EXIT_FAILURE);
-	for (int i = 0; i < NBR_ZOMBIE; i++)
+	}
+
+	int nbr_zombie = strToInt(argv[1]);
+
+	Zombie *horde = zombieHorde(nbr_zombie, "zomzom");
+
+	if (nbr_zombie > 0)
+	{
+		std::cout << std::endl;
+		std::cout << YELLOW "use the pointer on horde" RESET << std::endl;
+		horde->announce();
+	}
+
+	std::cout << std::endl;
+	std::cout << YELLOW "use each cell of the array" RESET << std::endl;
+	for (int i = 0; i < nbr_zombie; i++)
 		horde[i].announce();
+
+	std::cout << std::endl;
 	delete []horde;
 	return (EXIT_SUCCESS);
 }
