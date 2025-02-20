24f6be2a1de89fbd00dc4cb9be66825c0f085edf
Stephane Vogrig <svogrig@3E6.42angouleme.fr>
2025-02-20 13:41:10 +0100

    ex06 complete error message

--- ex06/main.cpp
+++ ex06/main.cpp
@@ -6,22 +6,29 @@
 /*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 22:15:32 by svogrig           #+#    #+#             */
-/*   Updated: 2025/02/19 21:21:05 by svogrig          ###   ########.fr       */
+/*   Updated: 2025/02/20 13:39:20 by svogrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <iostream>
-#include "Harl.hpp"
+# include <stdlib.h>
+# include <iostream>
+# include "Harl.hpp"
+
+# define RED "\033[31m"
+# define GREEN "\033[32m"
+# define YELLOW "\033[33m"
+# define RESET "\033[0m"
 
 int main(int argc, char **argv)
 {
 	if (argc != 2)
 	{
-		std::cout << "Error: wrong number of argument" << std::endl;
+		std::cerr << RED "Error:" YELLOW " wrong number of arguments !" RESET << std::endl;
+		std::cerr << YELLOW "Usage: "  RESET << argv[0] << " <level>" << std::endl;
+		std::cerr << YELLOW "level: DEBUG, INFO, WARNING, ERRROR"  RESET << std::endl;
 		return (EXIT_FAILURE);
 	}
-	
+
 	Harl harl;
 	int i = harl.levelToInt(argv[1]);
 	switch (i)
