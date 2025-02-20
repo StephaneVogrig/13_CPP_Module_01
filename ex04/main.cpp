2452bcecc39ca791c4e6724d5b1e6d36befe0c6e
Stephane Vogrig <svogrig@3E6.42angouleme.fr>
2025-02-20 13:36:07 +0100

    ex04 add .hpp and comment

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,27 +6,11 @@
 /*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 12:01:37 by svogrig           #+#    #+#             */
-/*   Updated: 2025/02/17 20:57:09 by svogrig          ###   ########.fr       */
+/*   Updated: 2025/02/20 13:32:18 by svogrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <iostream>
-#include <fstream>
-#include <string>
-
-#ifndef RED
-# define RED "\033[31m"
-#endif
-#ifndef GREEN
-# define GREEN "\033[32m"
-#endif
-#ifndef YELLOW
-# define YELLOW "\033[33m"
-#endif
-#ifndef RESET
-# define RESET "\033[0m"
-#endif
+#include "mysed.hpp"
 
 std::string replaceSubstring(const std::string& input, const std::string& search, const std::string& replace)
 {
@@ -49,10 +33,11 @@ int main(int argc, char** argv)
 	if (argc != 4)
 	{
 		std::cerr << RED "Error:" YELLOW " wrong number of arguments !" RESET << std::endl;
-		std::cerr << YELLOW "Usage: "  RESET << argv[0] << "<filename> <s1> <s2>" << std::endl;
+		std::cerr << YELLOW "Usage: "  RESET << argv[0] << " <filename> <s1> <s2>" << std::endl;
 		return (EXIT_FAILURE);
 	}
 
+	// open input file
 	char* inputFilename = argv[1];
 	std::ifstream inputFile(inputFilename);
 	if (!inputFile.is_open())
@@ -61,6 +46,7 @@ int main(int argc, char** argv)
 		return (EXIT_FAILURE);
 	}
 
+	// open output file
 	std::string outputFilename = inputFilename;
 	outputFilename.append(".replace");
 	std::ofstream outputFile(outputFilename.c_str());
@@ -69,13 +55,16 @@ int main(int argc, char** argv)
 		std::cerr << RED "Error:"  YELLOW " can't open file: " RESET << argv[1] << std::endl;
 		return (EXIT_FAILURE);
 	}
-	
+
 	std::string s1 = argv[2];
 	std::string s2 = argv[3];
-	
+
+	// load input file
 	std::string inputContent;
 	std::getline(inputFile, inputContent, '\0');
 	inputFile.close();
+
+	// fill output file
 	outputFile << replaceSubstring(inputContent, s1, s2);
 	outputFile.close();
 
---
+++ ex04/mysed.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mysed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/20 13:04:25 by svogrig           #+#    #+#             */
+/*   Updated: 2025/02/20 13:06:09 by svogrig          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MYSED_HPP
+# define MYSED_HPP
+
+# include <stdlib.h>
+# include <iostream>
+# include <fstream>
+# include <string>
+
+# define RED "\033[31m"
+# define GREEN "\033[32m"
+# define YELLOW "\033[33m"
+# define RESET "\033[0m"
+
+#endif
